09446adfe42c5bb1e2d90a56e678043046128843
tenye725 <tenye725@gmail.com>
2026-05-14 16:30:07 +0900

    fullstack-note-app init

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8080',
   },
+  output: 'standalone'
 }
 
 export default nextConfig
